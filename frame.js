b42d2865932bb02cba700250b9a0a4b46b481c57
ecc521 <42424091+ecc521@users.noreply.github.com>
2022-03-11 23:13:59 -0500

    Update frame.js
    Initial composer

--- frame.js
+++ frame.js
@@ -4,13 +4,40 @@ const contacts = {
 	"parents@stopclassrank.com": "Test Email",
 }
 
-
-const studentClassOptions = ["2022", "2023", "2024", "2025", "2026"]
+const studentClassOptions = ["2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029"]
 const personOptions = ["Parent", "Staff Member", "Student"]
+
+let reasonsForClassRank = [
+	"I was never informed about class rank",
+	"I thought class rank had been replaced with Latin Honors",
+	"I thought class rank did not matter",
+	"Primary courses selections were denied",
+	"Advice to not use PC19",
+	"Advice to not take AP courses freshman year",
+	"Advice to take fewer APs for a balanced schedule",
+	"Selecting interesting courses",
+	"Selecting courses for career goals",
+	"Not placing out of introductory language courses",
+	"Excessive Competitiveness of High School",
+	"Taking fewer APs due to extracurriculars",
+	"Rejected Schedule Changes",
+]
+
+let classTranslations = {
+	"2022": "Senior",
+	"2023": "Junior",
+	"2024": "Sophomore",
+	"2025": "Freshman",
+	"2026": "8th Grader",
+	"2027": "7th Grader",
+	"2028": "6th Grader",
+	"2029": "5th Grader",
+}
 
 
 let json = {
-	//showNavigationButtons: false,
+	showNavigationButtons: false,
+	checkErrorsMode: "onValueChanged",
 	showQuestionNumbers: "off",
 	"elements": [
 		{
@@ -32,12 +59,20 @@ let json = {
 			autoComplete: "name"
 		},
 
+
+		{
+			name: "address",
+			type: "text",
+			title: "I reside at",
+			placeHolder: "Please enter your address (optional)...",
+			autoComplete: "address"
+		},
+
 		{
 			type: "dropdown",
-			name: "grade",
+			name: "class",
 			visibleIf: "{personType} == 'Student'",
 			title: "I am in the class of ",
-			hasOther: true,
 			isRequired: true,
 			choices: studentClassOptions,
 		},
@@ -61,10 +96,9 @@ let json = {
 			isRequired: true,
 			columns: [
 				{
-					name: "grade",
+					name: "class",
 					title: "Class of",
 					isRequired: true,
-					hasOther: true,
 					choices: studentClassOptions
 				},
 				{
@@ -76,143 +110,149 @@ let json = {
 				},
 			]
 		},
+
+		{
+			name: "reasons",
+			type: "checkbox",
+			title: "Which of the following have negatively impacted you/your childrens' class rank?",
+			colCount: 1,
+			choices: reasonsForClassRank
+		},
+
 		{
             type: "editor",
-            name: "ckeditor",
-            title: "CK Editor"
+            name: "editor",
+            title: "Output Message"
         },
 	]
 };
+
+
+
+//Did your mid-year class rank surprise you
 
 Survey.StylesManager.applyTheme("modern");
 
 window.survey = new Survey.Model(json);
 
+//Auto-save survey
+const surveySaveKey = "surveyData"
+let lastDispatched = -Infinity;
 
+survey.onValueChanged = {fire: function(e) {
+	surveyChanged()
+	if (e.name !== "editor") {
+		updateSurveyMessage()
+	}
+}}
 
-survey
-.onComplete
-.add(function (sender) {
-	document
-	.querySelector('#surveyResult')
-	.textContent = "Result JSON:\n" + JSON.stringify(sender.data, null, 3);
-});
+function saveSurveyToDisk() {
+	localStorage.setItem(surveySaveKey, JSON.stringify(survey.data))
+}
+
+//This is the biggie - auto-save before unload.
+//We adopt a tick-mechanism as backup.
+window.addEventListener("beforeunload", saveSurveyToDisk)
+
+function surveyChanged() {
+	const dispatchInterval = 5000
+	if (Date.now() - lastDispatched > dispatchInterval) {
+		lastDispatched = Date.now()
+		setTimeout(function() {
+			saveSurveyToDisk()
+		}, dispatchInterval)
+	}
+}
+
+try {
+	//Not sure when this would error, but wrap just in case.
+	let data = localStorage.getItem(surveySaveKey)
+	if (data) {
+		survey.data = JSON.parse(data)
+	}
+}
+catch (e) {console.error(e)}
+//End auto-save survey
+
+updateSurveyMessage()
 
 ReactDOM.render(
 	React.createElement(SurveyReact.Survey, {model: survey})
 	, document.getElementById("surveyElement"));
 
 
+function updateSurveyMessage() {
+	let editor = survey.getQuestionByName("editor")
+	editor.value = generateMessage()
+}
 
 
+function generateMailto({
+	toField,
+	ccField,
+	subject,
+	body,
+}) {
+	let mailto = `mailto:${toField}?body=${encodeURIComponent(body)}&subject=${encodeURIComponent(subject)}&cc=${ccField.join(",")}`
+	return mailto
+}
 
 
+function generateMessage() {
+	let data = survey.data
 
+	let name = data.name
+	let address = data.address
 
-	function generateMailto({
-		toField,
-		ccField,
-		subject,
-		body,
-	}) {
-		let mailto = `mailto:${toField}?body=${encodeURIComponent(body)}&subject=${encodeURIComponent(subject)}&cc=${ccField.join(",")}`
-		return mailto
+	let typeOfPerson = data.personType?.toLowerCase()
+
+	let school = data.school
+
+	let message = `My name is ${name} and I${address?` reside at ${address}. I `:""} am a `
+
+	let children = survey.data.children || []
+
+	if (typeOfPerson === "parent") {
+		message += `parent with `
+		for (let i=0;i<children.length;i++) {
+			let child = children[i]
+			message += ` a ${classTranslations[child.class]} at ${child.school}`
+			if (i != children.length - 1 && children.length >= 2) {
+				if (children.length > 2) {
+					message += ", "
+				}
+				else if (children.length === 2) {
+					message += " "
+				}
+			}
+			if (i == children.length - 2 && children.length > 1) {
+				message += "and "
+			}
+		}
+		message += ". "
+	}
+	else if (typeOfPerson === "student") {
+		message += `${classTranslations[data.class]} at ${school}. `
+	}
+	else {
+		message += `${typeOfPerson} at ${school}. `
 	}
 
-
-	function generateMessage({
-		sendToEmail,
-		name,
-		address,
-		typeOfPerson,
-		school,
-	}) {
-		let addressee = contacts[sendToEmail]
-		typeOfPerson = typeOfPerson.toLowerCase()
-
-		let message = `Dear ${addressee}
-
-		My name is ${name} and I${address?` reside at ${address}. I `:""} am a ${typeOfPerson === "parent" ? " parent of a student":typeOfPerson} at ${school}.
-		`
-
-		return message
+	if (data.reasons?.length) {
+		message += `<br><br>The following problems negatively impacted ${typeOfPerson === "parent" ? `my ${children.length > 1 ? "children's" : "child's"}` : "my"} class rank: `
+		for (let i=0;i<data.reasons.length;i++) {
+			message += `<br> - ${data.reasons[i]}`
+		}
 	}
 
+	return message
+}
 
 
-
-
-	//
-	// function createDropdown(optionsList, placeHolder) {
-	// 	let select = document.createElement("select")
-	// 	optionsList.forEach((option) => {
-	// 		let optionElem = document.createElement("option")
-	// 		optionElem.innerHTML = option
-	// 		select.appendChild(optionElem)
-	// 	})
-	// 	return select
-	// }
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	// let inputForm = document.createElement("div")
-	// document.body.appendChild(inputForm)
-	// inputForm.className = "inputForm"
-	//
-	//
-	//
-	//
-	//
-
-	//
-	// let message = generateMessage({
-	// 	sendToEmail: "parents@stopclassrank.com",
-	// 	name: "Bob",
-	// 	typeOfPerson: personOptions[3],
-	// 	//address: "",
-	// 	school: "Green Level High School"
-	// })
-	//
-	// console.log(message)
-	//
-	//
-	// console.log(message)
-	//
-	// let a = document.createElement("a")
-	// a.href = generateMailto({
-	// 	toField: "parents@stopclassrank.com",
-	// 	ccField: ["student@stopclassrank.com", "info@stopclassrank.com"],
-	// 	subject: "Class Rank Concerns",
-	// 	body: message
-	// })
-	// a.innerHTML = "Message"
-	// inputForm.appendChild(a)
-	//
-
-
-
-
-
-
-
-
-	//
-	//
-	//
-	// const template = `
-	// Dear ${addressee}
-	//
-	// My name is and I ${name}${address?` reside at ${address}. I `:""} am currently a ${currentClass} at ${school}.
-	// `
-	//
-
-
+setTimeout(function() {
+	//Attempt to enlarge textarea.
+	document.getElementById("cke_1_contents").style.height = "80vh"
+}, 2000)
 
 
 	/*
